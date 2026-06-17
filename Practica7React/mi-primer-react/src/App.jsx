7c72086f9e17440b9db87f0f60fa35f66c8c76c9
Enyel8989 <angel.lopez2026@merlet.superate.org.sv>
2026-06-16 22:19:26 -0600

    Practica 7 react

---
+++ Practica7React/mi-primer-react/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import MiComponente from './MiComponente'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <section id='center'></section>
+    <h1>Ideas de planes de fin de semana</h1>
+    <MiComponente 
+      titulo= "Un atol chuco del centro"
+      lugar= "SanSalvador"
+      />
+      <MiComponente 
+      titulo= "Viernes de amigos"
+      lugar= "San Miguel"
+      />
+      <MiComponente 
+      titulo= "Explorando el pasado"
+      lugar= "Santa Ana"
+      />
+    </>
+  )
+}
+
+export default App
---
+++ Practica7React/mi-primer-react/src/MiComponente.jsx
@@ -0,0 +1,8 @@
+export default function MiComponente() {
+    return(
+        <section className="card">
+            <h2 className="card-tittle">{titulo}</h2>
+            <h3 className="card-subtittle">{lugar}</h3>
+        </section>
+    )
+}
